920c502170e511e47f1a36938c4c77bba1ad58d8
atishyasaxena1 <atishyasaxena1@gmail.com>
2025-11-13 12:04:04 +0530

    Create user_input_example.py

---
+++ user_input_example.py
@@ -0,0 +1,6 @@
+name = input("name: ")
+age = int(input("age: "))
+bus_fare = float(input("bus fare: "))
+
+print("My name is", name, ", I am", age, "years old and I pay $", bus_fare, "for one way to the university.")
+
